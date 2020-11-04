0e51abad9f630fea0b38244937658cd159bab404
Monitor Laboratório 1 <monitorlab1@unifacef.edu.br>
2020-11-04 20:35:35 -0300

    api mercado pago

---
+++ mercado_pago/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const MercadoPago = require('mercadopago')
+
+const app = express()
+
+MercadoPago.configure({
+    // serve para dizer se está em desenvolvimento ou produção
+    // true quer dizer que está em desenvolvimento
+    sandBox: true,
+    access_token: 'TEST-1184271242648905-110422-06fb1475eb7ae61d39a879659edf98d7-223205371'
+})
+
+app.get('/', (req, res) => {
+    res.send('ppa')
+})
+
+app.get('/pagar',  async (req, res) => {
+    const id = String(Date.now())
+    const email = 'joaodev3@gmail.com'
+
+    var dados = {
+        items: [
+            item = {
+                id: id,
+                title: 'bone, relogio',
+                quantity: 1,
+                currency_id: 'BRL',
+                unit_price: parseFloat(50)
+            }
+        ],
+        payer: {
+            email: email
+        },
+        external_reference: id
+    }
+
+    try{
+        var pagamento = await MercadoPago.preferences.create(dados)
+    console.log(pagamento)
+    return res.redirect(pagamento.body.init_point)
+    }
+    catch(err){
+        return res.send(err.message)
+    }
+    
+})
+
+app.listen(3000, console.log('server'))
